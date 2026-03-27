4fed3f700e7e8ef6b13c60a6d3aeda412d549bd2
NexTUI <libyaball0@gmail.com>
2026-03-27 21:52:42 +0200

    Update style.sh

--- arch/waybar/themes/Dynamic-Island/style.sh
+++ arch/waybar/themes/Dynamic-Island/style.sh
@@ -3,10 +3,10 @@ HYPR_CONF="$HOME/.config/hypr/hyprland.conf"
 ROFI_CONF="$HOME/.config/rofi/config.rasi"
 if [ -f "$HYPR_CONF" ]; then
   sed -i -E \
-    -e 's/gaps_in = [0-9]*/gaps_in = 2/' \
-    -e 's/gaps_out = [0-9]*/gaps_out = 3/' \
+    -e 's/gaps_in = [0-9]*/gaps_in = 7/' \
+    -e 's/gaps_out = [0-9]*/gaps_out = 12/' \
     -e 's/border_size = [0-9]*/border_size = 3/' \
-    -e 's/rounding = [0-9]*/rounding = 8/' \
+    -e 's/rounding = [0-9]*/rounding = 24/' \
     -e 's/enabled = false/enabled = true/' "$HYPR_CONF"
 fi
 if [ -f "$ROFI_CONF" ]; then
